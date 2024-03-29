30430c2cbf1201b5708b3a9b279a5d269fdc4fe2
Carolina-Sichaca <134023074+Carolina-Sichaca@users.noreply.github.com>
2024-03-28 22:33:01 -0500

    scrip seleccion de personajes
    se cea scrip para seleccionar personaje en el menu principal

---
+++ Assets/Script/GuardarPersonaje.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardarPersonaje : MonoBehaviour
+
+{
+    public bool Gato;
+    public bool Perro;
+    public bool Conejo;
+
+    private void Update()
+    {
+        if (Gato == false && Perro == false && Conejo == false)
+        {
+            Gato = true;
+        }
+        Gato= PlayerPrefs,GetInt("seleccionGato") == 1;
+        Perro= PlayerPrefs,GetInt("seleccionPerro") == 1;
+        Conejo= PlayerPrefs,GetInt("seleccionConejo") == 1;
+    }
+
+    public void PersonajeGato()
+    {
+        Gato = true;
+        Perro = false;
+        Conejo = false;
+        Guardar();
+
+    }
+
+    public void PersonajePerro()
+    {
+        Gato = false;
+        Perro = true;
+        Conejo = false;
+        Guardar();
+
+    }
+
+     public void PersonajeConejo()
+    {
+        Gato = false;
+        Perro = false;
+        Conejo = true;
+        Guardar();
+
+    }
+
+    public void Guardar()
+    {
+        PlayerPrefs.SetInt("seleccionGato", Gato ? 1: 0);
+        PlayerPrefs.SetInt("seleccionPerro", Perro ? 1: 0);
+        PlayerPrefs.SetInt("seleccionConejo", Conejo ? 1: 0);
+    }
+}
