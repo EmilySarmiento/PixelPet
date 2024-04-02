4015f255376498cbe2fa990a0c7674ad40af5cdf
Emily Sarmiento <90357648+EmilySarmiento@users.noreply.github.com>
2024-04-02 18:24:08 -0500

    cambios minijuego
    se agrego el movimiento de personajes en el minijuego

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed; //Velocidad
+    public float jumpStreng; //Fuerza del salto
+    public int maxJumps; //Máximo de saltos
+    public LayerMask capaSuelo;
+    public AudioClip sonidoSalto;
+
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D boxcollider;
+    private bool mirandoDerecha = true;
+    private int lessJumps; //Saltos restantes
+    //private Animator animator;
+    // Update is called once per frame
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxcollider = GetComponent<BoxCollider2D>();
+        //animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        
+        procesarMovimiento();
+        ProcesarSalto();
+    }
+
+    bool EstaEnSuelo()
+    {
+        RaycastHit2D raycasthit = Physics2D.BoxCast(boxcollider.bounds.center, new Vector2(boxcollider.bounds.size.x, boxcollider.bounds.size.y), 0f, Vector2.down, 0.2f, capaSuelo);
+        return raycasthit.collider != null;
+    }
+    void ProcesarSalto()
+    {
+        if (EstaEnSuelo())
+        {
+            lessJumps = maxJumps;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && lessJumps > 0)
+        {
+            lessJumps = lessJumps - 1;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
+            rigidBody.AddForce(Vector2.up * jumpStreng, ForceMode2D.Impulse);
+        }
+    }
+
+    void procesarMovimiento()
+    {
+        //Lógica de movimiento//
+        //Revisa la tecla que esté presinando el jugador
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(inputMovimiento * speed, rigidBody.velocity.y);
+        gestionarOrientacion(inputMovimiento);
+    }
+
+    void gestionarOrientacion(float inputMovimiento)
+    {
+        //Si se cumple la condición
+        if ((mirandoDerecha == true && inputMovimiento < 0)||(mirandoDerecha == false && inputMovimiento > 0))
+        {
+            //Voltear al personaje
+            mirandoDerecha = !mirandoDerecha;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+
+    }
+    
+}
