1b4205b5ee3ca22af3dd4dcd7e7f3666094bcb6e
NatlySang02 <natlytrabajos@gmail.com>
2024-05-14 20:09:19 -0500

    Intento de boton comida
    aumentar, no se ejecuta

--- Assets/Script/BarraComida.cs
+++ Assets/Script/BarraComida.cs
@@ -8,18 +8,16 @@ public class BarraComida : MonoBehaviour
     public int comidaMax;
     public float comidaActual;
     public Image ImagenBarraComida;
+    public Button Buttoncomida;
 
     public float ChargeRate;
 
-
     public bool comidaAlMaximo = false;
-
 
     void Start()
     {
         comidaActual = comidaMax;
     }
-
 
     void Update()
     {
@@ -31,22 +29,28 @@ public class BarraComida : MonoBehaviour
         }
     }
 
-
     public void RevisarComida()
     {
         ImagenBarraComida.fillAmount = comidaActual / comidaMax;
     }
 
+    public void SubirComida() // Add this method
+    {
+        if (!comidaAlMaximo)
+        {
+             comidaActual += ChargeRate + 10f;
+        }
+    }
+
     private IEnumerator BajarBarra()
     {
-       yield return new WaitForSeconds(1f);
-       while(comidaActual < comidaMax)
-       {
-        comidaActual += ChargeRate / 10f;
-        if(comidaActual > comidaMax) comidaActual = comidaMax;
-       ImagenBarraComida.fillAmount = comidaActual / comidaMax;
-        yield return new WaitForSeconds(.1f);
-       }
-}
-
+        yield return new WaitForSeconds(1f);
+        while (comidaActual < comidaMax)
+        {
+            comidaActual += ChargeRate / 10f;
+            if (comidaActual > comidaMax) comidaActual = comidaMax;
+            ImagenBarraComida.fillAmount = comidaActual / comidaMax;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 }
